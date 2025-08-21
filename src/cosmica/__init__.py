de0765b90e0f62100fce72481239c5a9b9bf6b7f
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2025-08-21 17:13:13 +0900

    Rename package name to cosmica

--- src/cosmica/__init__.py
+++ src/cosmica/__init__.py
@@ -1,4 +1,4 @@
-"""Package initialization module for package-name-goes-here."""
+"""Package initialization module for cosmica."""
 
 from typing import Annotated
 
@@ -7,7 +7,7 @@ from typing_extensions import Doc
 
 def main() -> None:
     """Print a greeting message."""
-    print("Hello from package-name-goes-here!")  # noqa: T201
+    print("Hello from cosmica!")  # noqa: T201
 
 
 def square(x: Annotated[int, Doc("The integer to square.")]) -> Annotated[int, Doc("The square of the integer.")]:

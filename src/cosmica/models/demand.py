2af84cc35f97fe4746349d50823990927c7a5bd8
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2025-10-29 14:17:28 +0900

    Remove deprecated TOML load functions and methods

--- src/cosmica/models/demand.py
+++ src/cosmica/models/demand.py
@@ -25,13 +25,6 @@ class Demand[T: Hashable](ABC):
 
     id: T
 
-    @classmethod
-    @abstractmethod
-    @deprecated("Construction of objects from TOML files is deprecated and will be removed in future versions.")
-    def parse_demand_item(cls, item: Mapping[str, Any]) -> Self:
-        """Parse a demand item."""
-        ...
-
 
 @dataclass(frozen=True, kw_only=True, slots=True)
 class ConstantCommunicationDemand[T: Hashable](Demand[T]):
@@ -42,17 +35,6 @@ class ConstantCommunicationDemand[T: Hashable](Demand[T]):
     destination: NodeGID
     distribution: Literal["uniform", "poisson"]
     transmission_rate: float
-
-    @classmethod
-    @deprecated("Construction of objects from TOML files is deprecated and will be removed in future versions.")
-    def parse_demand_item(cls, item: Mapping[str, Any]) -> Self:
-        return cls(
-            id=item["id"],
-            source=item["src_gid"],
-            destination=item["dst_gid"],
-            transmission_rate=item["transmission_rate_bps"],
-            distribution=item["distribution"],
-        )
 
 
 @dataclass(frozen=True, kw_only=True, slots=True)
@@ -73,19 +55,6 @@ class TemporaryCommunicationDemand[T: Hashable](Demand[T]):
     ) -> np.bool:
         return self.start_time <= current_time < self.end_time
 
-    @classmethod
-    @deprecated("Construction of objects from TOML files is deprecated and will be removed in future versions.")
-    def parse_demand_item(cls, item: Mapping[str, Any]) -> Self:
-        return cls(
-            id=item["id"],
-            source=item["src_gid"],
-            destination=item["dst_gid"],
-            transmission_rate=item["transmission_rate_bps"],
-            distribution=item["distribution"],
-            start_time=np.datetime64(item["start_time"].astimezone(tz=UTC).replace(tzinfo=None)),
-            end_time=np.datetime64(item["end_time"].astimezone(tz=UTC).replace(tzinfo=None)),
-        )
-
 
 @dataclass(frozen=True, kw_only=True, slots=True)
 class OneTimeCommunicationDemand[T: Hashable](Demand[T]):
@@ -103,38 +72,3 @@ class OneTimeCommunicationDemand[T: Hashable](Demand[T]):
     data_size: float
     generation_time: np.datetime64
     deadline: np.datetime64
-
-    @classmethod
-    @deprecated("Construction of objects from TOML files is deprecated and will be removed in future versions.")
-    def parse_demand_item(cls, item: Mapping[str, Any]) -> Self:
-        return cls(
-            id=item["id"],
-            source=item["src_gid"],
-            destination=item["dst_gid"],
-            data_size=item["data_size"],
-            generation_time=item["generation_time"],
-            deadline=item["deadline"],
-        )
-
-
-_DEMAND_TYPES: dict[str, type[Demand[Hashable]]] = {
-    "constant": ConstantCommunicationDemand,
-    "temporary": TemporaryCommunicationDemand,
-    "one_time": OneTimeCommunicationDemand,
-}
-
-
-@deprecated("Construction of objects from TOML files is deprecated and will be removed in future versions.")
-def parse_demand_item(item: Mapping[str, Any]) -> Demand[Hashable]:
-    """Parse a demand item."""
-    demand_type = item["type"]
-    return _DEMAND_TYPES[demand_type].parse_demand_item(item)
-
-
-@deprecated("Construction of objects from TOML files is deprecated and will be removed in future versions.")
-def load_demands_from_toml_file(toml_file_path: str | Path) -> list[Demand[Hashable]]:
-    """Load demands from a TOML file."""
-    toml_file_path = Path(toml_file_path)
-    with toml_file_path.open("rb") as f:
-        toml_data = tomllib.load(f)
-    return list(map(parse_demand_item, toml_data["demands"]))
--- src/cosmica/models/gateway.py
+++ src/cosmica/models/gateway.py
@@ -4,6 +4,7 @@ __all__ = [
     "Gateway",
     "GatewayOGS",
 ]
+
 import tomllib
 from collections.abc import Hashable
 from dataclasses import dataclass, field
@@ -31,22 +32,6 @@ class Gateway[T: Hashable](Node[T]):
     n_terminals: int = field(default=1, compare=False)
 
     @classmethod
-    @deprecated("Construction of objects from TOML files is deprecated and will be removed in future versions.")
-    def from_toml_file(cls, toml_file_path: Path | str) -> list[Self]:
-        toml_file_path = Path(toml_file_path)
-        with toml_file_path.open("rb") as f:
-            toml_data = tomllib.load(f)
-        return list(map(cls.parse_gateway_item, toml_data["gateways"]))
-
-    @classmethod
-    @deprecated("Construction of objects from TOML files is deprecated and will be removed in future versions.")
-    def parse_gateway_item(cls, item: MutableMapping[str, Any]) -> Self:
-        item["latitude"] = np.deg2rad(item.pop("lat_deg"))
-        item["longitude"] = np.deg2rad(item.pop("lon_deg"))
-        item["minimum_elevation"] = np.deg2rad(item.pop("min_el_deg"))
-        return cls(**item)
-
-    @classmethod
     @override
     def class_name(cls) -> str:
         return "GW"
@@ -62,25 +47,6 @@ class GatewayOGS[T: Hashable](Node[T]):
     n_terminals: int = field(default=1, compare=False)
     aperture_size: float = field(default=1.0, compare=False)
     rytov_variance: float = field(default=0.5, compare=False)
-
-    @classmethod
-    @deprecated("Construction of objects from TOML files is deprecated and will be removed in future versions.")
-    def from_toml_file(cls, toml_file_path: Path | str) -> list[Self]:
-        toml_file_path = Path(toml_file_path)
-        with toml_file_path.open("rb") as f:
-            toml_data = tomllib.load(f)
-        return list(map(cls.parse_gateway_item, toml_data["gateways"]))
-
-    @classmethod
-    @deprecated("Construction of objects from TOML files is deprecated and will be removed in future versions.")
-    def parse_gateway_item(cls, item: MutableMapping[str, Any]) -> Self:
-        item["latitude"] = np.deg2rad(item.pop("lat_deg"))
-        item["longitude"] = np.deg2rad(item.pop("lon_deg"))
-        item["minimum_elevation"] = np.deg2rad(item.pop("min_el_deg"))
-        item["aperture_size"] = item.pop("aperture_size_m")
-        item["rytov_variance"] = item.pop("rytov_variance")
-
-        return cls(**item)
 
     @classmethod
     @override

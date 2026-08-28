2d56f6788710006366fe646c007294e314d37786
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2026-08-28 13:52:52 +0900

    Use shared factories for assigned link tests

--- tests/dtos/test_communication_link.py
+++ tests/dtos/test_communication_link.py
@@ -1,51 +1,15 @@
-import numpy as np
 import pytest
 
 from cosmica.dtos import CommunicationLinkEndpoint, DirectedCommunicationLink
-from cosmica.models import (
-    CircularSatelliteOrbitModel,
-    ConstellationSatellite,
-    Gateway,
-    OpticalCommunicationTerminal,
-)
-from cosmica.utils.constants import EARTH_RADIUS
-
-EPOCH = np.datetime64("2026-01-01T00:00:00")
-
-
-def _make_terminal(terminal_id: int) -> OpticalCommunicationTerminal[int]:
-    return OpticalCommunicationTerminal(
-        id=terminal_id,
-        azimuth_min=-np.pi,
-        azimuth_max=np.pi,
-        elevation_min=-np.pi / 2,
-        elevation_max=np.pi / 2,
-        angular_velocity_max=1.0,
-    )
-
-
-def _make_satellite(
-    satellite_id: int,
-    terminals: tuple[OpticalCommunicationTerminal[int], ...],
-) -> ConstellationSatellite[int]:
-    return ConstellationSatellite(
-        id=satellite_id,
-        orbit=CircularSatelliteOrbitModel(
-            semi_major_axis=EARTH_RADIUS + 1_000e3,
-            inclination=0.0,
-            raan=0.0,
-            phase_at_epoch=0.0,
-            epoch=EPOCH,
-        ),
-        terminals=terminals,
-    )
+from cosmica.models import Gateway
+from tests.factories import make_satellite, make_terminal
 
 
 def test_directed_link_identifies_nodes_and_assigned_terminals() -> None:
-    source_terminal = _make_terminal(1)
-    destination_terminal = _make_terminal(2)
-    source_satellite = _make_satellite(1, (source_terminal,))
-    destination_satellite = _make_satellite(2, (destination_terminal,))
+    source_terminal = make_terminal(1)
+    destination_terminal = make_terminal(2)
+    source_satellite = make_satellite(1, terminals=(source_terminal,))
+    destination_satellite = make_satellite(2, terminals=(destination_terminal,))
 
     link = DirectedCommunicationLink(
         source=CommunicationLinkEndpoint(node=source_satellite, terminal=source_terminal),
@@ -59,9 +23,9 @@ def test_directed_link_identifies_nodes_and_assigned_terminals() -> None:
 
 
 def test_endpoint_rejects_terminal_owned_by_another_node() -> None:
-    owned_terminal = _make_terminal(1)
-    other_terminal = _make_terminal(2)
-    satellite = _make_satellite(1, (owned_terminal,))
+    owned_terminal = make_terminal(1)
+    other_terminal = make_terminal(2)
+    satellite = make_satellite(1, terminals=(owned_terminal,))
 
     with pytest.raises(ValueError, match="is not owned by node"):
         CommunicationLinkEndpoint(node=satellite, terminal=other_terminal)
@@ -76,4 +40,4 @@ def test_endpoint_rejects_node_without_terminal_inventory() -> None:
     )
 
     with pytest.raises(TypeError, match="does not own communication terminals"):
-        CommunicationLinkEndpoint(node=gateway, terminal=_make_terminal(1))
+        CommunicationLinkEndpoint(node=gateway, terminal=make_terminal(1))
